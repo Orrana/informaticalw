b55a9088350eebfb5f7bae4a240b2cf269fcde86
Orrana <43647183+Orrana@users.noreply.github.com>
2019-10-13 21:55:00 -0300

    versão de domingo Larissa

---
+++ aplicacao.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+ 
+require_once 'conexao.php';
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+ 
+        <title>Painel | Informatica LW</title>
+        <link rel="stylesheet" href="_estilos-CSS/estilosaplicacao.css">
+    </head>
+ 
+    <body>
+         
+        
+        <div style="background-color:black;color:white;padding:20px;">
+        <h1 id="inicial">BEM VINDO À INFORMÁTICA LW</h1>
+        <p id="linha-config">
+                <?php 
+                     $nomeusuario=$_POST['email'];
+                        echo "{$nomeusuario} seja bem vindo(a)!";
+                
+                ?>
+                <a href="index.html"><button class="btn btn-lg btn-primary btn-block">Logout</button></a>
+                
+                <button class="btn btn-lg btn-primary btn-block">Configurações</button></p></div>
+        <div id="header">
+            <button class="btn btn-lg btn-primary btn-block">Histórico de compras</button>
+            
+        </div>
+        <div id="conteudo-left"></div>
+        <div id="pasta"></div>
+        <div id="resto"></div>
+    </body>
+</html>
