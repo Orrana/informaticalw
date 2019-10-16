fb18807dc804827ee0ccc035e66e4be834831079
miroclouse <48926190+miroclouse@users.noreply.github.com>
2019-10-15 22:42:34 -0300

    versão weslley terça - corrigindo

--- aplicacao.php
+++ aplicacao.php
@@ -15,8 +15,8 @@
         <h1 id="inicial">BEM VINDO À INFORMÁTICA LW</h1>
         <p id="linha-config">
                 <?php 
-                     
-                        echo $_SESSION['nome_cliente']."seja bem vindo(a)!";
+                     $nomeusuario=$_POST['email'];
+                        echo "{$nomeusuario} seja bem vindo(a)!";
                 
                 ?>
                 <a href="index.html"><button class="btn btn-lg btn-primary btn-block">Logout</button></a>
@@ -29,7 +29,6 @@
             <button class="btn btn-lg btn-primary btn-block">Histórico de compras</button>
             
         </div>
-<<<<<<< HEAD
         <div id="conteudo-left" ></div>
         <div id="pasta">
             <iframe id='Iframe' src="restaurar.html" height="700" width="100%"
@@ -37,26 +36,6 @@
             
 
             
-=======
-        <div id="conteudo-left"></div>
-        <div id="pasta">
-            
-<<<<<<< HEAD
-=======
-            echo $_SESSION['senha'];
-            echo $usuario['id_cliente'];
-            echo $usuario['nome_cliente'];
-            echo $usuario['telefone_cliente'];
-            echo $usuario['endereco_cliente'];
-              
-               echo $_SESSION['usuario'];
-               echo $_SESSION['id_cliente'];
-               echo  $_SESSION['nome_cliente'];
-               echo  $_SESSION['telefone_cliente'];
-               echo  $_SESSION['endereco_cliente'];
-            ?>
->>>>>>> 56d1c28cec01a9703236b8ae7c7ffac5c71635a1
->>>>>>> 9c64b6d6072a4df8cb11db4782f47cb528fad82d
         </div>
         <div id="resto"></div>
     </body>
